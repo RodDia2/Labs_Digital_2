63d4dea61ebf108ba9b10a4ea5b8517c166f7e3c
RodDia2 <78278871+RodDia2@users.noreply.github.com>
2021-02-22 08:16:48 -0600

    master parte 1 terminada

--- Mini_1/Mini1_Master.X/main_master.c
+++ Mini_1/Mini1_Master.X/main_master.c
@@ -3,7 +3,7 @@
  * File:   main.c
  * Author: Pablo
  * Ejemplo de implementación de la comunicación SPI 
- * Código Esclavo
+ * Código Maestro
  * Created on 10 de febrero de 2020, 03:32 PM
  */
 //*****************************************************************************
@@ -34,69 +34,60 @@
 //*****************************************************************************
 #include <xc.h>
 #include <stdint.h>
-#include <pic16f887.h>
-
-
 #include "SPI.h"
-#include "ADC.h"
 //*****************************************************************************
 // Definición de variables
 //*****************************************************************************
 #define _XTAL_FREQ 8000000
-uint8_t contador = 0;
-uint8_t valor_adc = 0;
+
+uint8_t temperatura = 0;
 //*****************************************************************************
 // Definición de funciones para que se puedan colocar después del main de lo 
 // contrario hay que colocarlos todas las funciones antes del main
 //*****************************************************************************
 void setup(void);
-//*****************************************************************************
-// Código de Interrupción 
-//*****************************************************************************
-void __interrupt() isr(void){
-   if(SSPIF == 1){
-        //PORTD = spiRead();
-        spiWrite(valor_adc);
-        SSPIF = 0;
-    }
-   if (PIR1bits.ADIF == 1) {
-        // se pasa el valor del ADRESH a la variable
-        valor_adc = ((ADRESH * 150)/255);
-        // se apaga la bandera
-        PIR1bits.ADIF = 0;
-    }
-   
-    
-   
-}
+
 //*****************************************************************************
 // Código Principal
 //*****************************************************************************
 void main(void) {
     setup();
-    initADC();
-    ADC_Select(0);
-    ADCON1bits.VCFG0 = 1;
     //*************************************************************************
     // Loop infinito
     //*************************************************************************
     while(1){
-        adc();
-        if (valor_adc < 25) {
-            PORTDbits.RD2 = 1;
-            PORTDbits.RD1 = 0;
-            PORTDbits.RD0 = 0;
-                  
-        } else if (25 <= valor_adc && valor_adc < 36) {
-            PORTDbits.RD2 = 0;
-            PORTDbits.RD1 = 1;
-            PORTDbits.RD0 = 0;
-        } else if (valor_adc >= 36) {
-            PORTDbits.RD2 = 0;
-            PORTDbits.RD1 = 0;
-            PORTDbits.RD0 = 1;
-        }
-
+       PORTCbits.RC2 = 0;       //Slave Select
+       __delay_ms(1);
+       
+       spiWrite(1);
+       PORTB = spiRead();
+       
+       __delay_ms(1);
+       PORTCbits.RC2 = 1;       //Slave Deselect 
+      
+       __delay_ms(1);
+       PORTCbits.RC1 = 0;
+       __delay_ms(1);
+       
+       spiWrite(1);
+       PORTD = spiRead();
+       
+       __delay_ms(1);
+       PORTCbits.RC1 = 1; 
+       __delay_ms(1);
+       
+       
+       
+       PORTCbits.RC0 = 0;       //Slave Select
+       __delay_ms(1);
+       
+       spiWrite(1);
+       temperatura = spiRead();
+       
+       __delay_ms(1);
+       PORTCbits.RC0 = 1;       //Slave Deselect 
+      
+       __delay_ms(1);
     }
     return;
 }
@@ -104,28 +95,18 @@ void main(void) {
 // Función de Inicialización
 //*****************************************************************************
 void setup(void){
-    ANSEL = 0b00000001;
+    ANSEL = 0;
     ANSELH = 0;
-    
-    TRISA = 0b00000001;
-    TRISB = 0b00000000;
-    TRISC = 0;
+    TRISC0 = 0;
+    TRISC1 = 0;
+    TRISC2 = 0;
+    TRISB = 0;
     TRISD = 0;
-    
-    PORTA = 0;
     PORTB = 0;
-    PORTC = 0;
     PORTD = 0;
-    
-    //ADCON1bits.VCFG0 = 1;
-    
-    INTCONbits.GIE = 1;         // Habilitamos interrupciones
-    INTCONbits.PEIE = 1;        // Habilitamos interrupciones PEIE
-    PIR1bits.SSPIF = 0;         // Borramos bandera interrupción MSSP
-    PIE1bits.SSPIE = 1;         // Habilitamos interrupción MSSP
-    TRISAbits.TRISA5 = 1;       // Slave Select
-   
-    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+    PORTCbits.RC2 = 1;
+    PORTCbits.RC1 = 1;
+    PORTCbits.RC0 = 1;
+    spiInit(SPI_MASTER_OSC_DIV4, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
 
 }
-
