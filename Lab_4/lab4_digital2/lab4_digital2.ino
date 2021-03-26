3680d782c1f21bc6358edc3132a3f0506cd7ca40
RodDia2 <78278871+RodDia2@users.noreply.github.com>
2021-03-26 17:17:43 -0600

    lab terminado

--- Lab_4/lab4_digital2/lab4_digital2.ino
+++ Lab_4/lab4_digital2/lab4_digital2.ino
@@ -15,22 +15,237 @@ int bandera2 = 0;
 int cont = 0;
 int cont2 = 0;
 
+int led1 = PB_5;
+int led2 = PB_0;
+int led3 = PB_1;
+int led4 = PE_4;
+int led5 = PE_5;
+int led6 = PB_4;
+int led7 = PA_5;
+int led8 = PA_6;
+
+int led9 = 38;
+int led10 = 37;
+int led11 = 36;
+int led12 = 35;
+int led13 = 34;
+int led14 = 33;
+int led15 = 32;
+int led16 = 11;
+
 void setup() {
   pinMode(ledVerde, OUTPUT);
   pinMode(ledRojo, OUTPUT);
   pinMode(ledAzul, OUTPUT);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
+  pinMode(led11, OUTPUT);
+  pinMode(led12, OUTPUT);
+  pinMode(led13, OUTPUT);
+  pinMode(led14, OUTPUT);
+  pinMode(led15, OUTPUT);
+  pinMode(led16, OUTPUT);
   
   pinMode(interruptPin, INPUT_PULLUP);
   pinMode(interruptPin2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), blink, LOW);
-  attachInterrupt(digitalPinToInterrupt(interruptPin2), blink2, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin2), blink2, LOW);
 }
 
 void loop() {
-//switch (cont) {
-//  case 0:
-  
-//  }
+switch (cont) {
+  case 0:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 1:
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 2:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 3:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 4:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 5:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 6:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, HIGH);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, LOW);
+    break;
+  case 7:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, HIGH);
+    digitalWrite(led8, LOW);
+    break;
+  case 8:
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(led7, LOW);
+    digitalWrite(led8, HIGH);
+    break;
+  }
+switch(cont2) {
+  case 0:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 1:
+    digitalWrite(led9, HIGH);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 2:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, HIGH);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 3:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, HIGH);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 4:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, HIGH);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 5:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, HIGH);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 6:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, HIGH);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, LOW);
+    break;
+  case 7:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, HIGH);
+    digitalWrite(led16, LOW);
+    break;
+  case 8:
+    digitalWrite(led9, LOW);
+    digitalWrite(led10, LOW);
+    digitalWrite(led11, LOW);
+    digitalWrite(led12, LOW);
+    digitalWrite(led13, LOW);
+    digitalWrite(led14, LOW);
+    digitalWrite(led15, LOW);
+    digitalWrite(led16, HIGH);
+    break;
+  }
 }
 
 void blink() {
@@ -53,12 +268,13 @@ void blink() {
     bandera2 = 1;
     bandera = 0;
   }
-  if (bandera == 0) {
+  else {
     cont2++;
     if (cont2 > 8) {
       cont2 = 0;
-      digitalWrite(ledRojo, HIGH);
+      digitalWrite(ledAzul, HIGH);
       bandera = 1;
+      bandera2 = 0;
       }
   }
  }
@@ -71,8 +287,9 @@ void blink2() {
     cont++;
     if (cont > 8) {
       cont = 0;
-      digitalWrite(ledAzul, HIGH);
+      digitalWrite(ledRojo, HIGH);
       bandera = 1;
+      bandera2 = 0;
       }
     }
   }
