b2ae46c0c13fe09901c762644b7ddbe18799d792
RodDia2 <78278871+RodDia2@users.noreply.github.com>
2021-05-25 13:16:19 -0600

    mini4 tiva se pueden encender leds con botones

--- Mini_4/mini_3/main.c
+++ Mini_4/mini_3/main.c
@@ -2,8 +2,75 @@
 
 /**
  * main.c
+ *  Rodrigo Díaz, 18265
+ * Digital II, mini3
  */
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/systick.h"
+#include "driverlib/uart.h"
+#include "driverlib/rom.h"
+#include <string.h>
+#include "grlib/grlib.h"
+#include "driverlib/pin_map.h"
+
+//*********variables*******************
+uint8_t numero = 0;
+
+//**********prototipos******************
+
+//void 7seg(uint8_t numero);
+
+//**********codigo principal************
 int main(void)
 {
-	return 0;
+    //configuracion del reloj: frecuencia de reloj de 40 MHz porque se uso el PLL
+    // se utiliza el oscilador principal
+    SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ );
+
+    // como solo se tiene el puerto B completo, se usara para el 7 segmentos por facilidad PINES 0-6
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){}
+    // pines del 0 al 7 se configuran como outputs
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6);
+
+    // en el puerto A se utilizara para los pushbuttons, PINES 2,3,4,5 con weak pull-up
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)){}
+    GPIOPinTypeGPIOInput(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5);
+    GPIOPadConfigSet(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
+
+    // se utilizara el puerto C para las leds verdes PINES 4,5,6,7
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC)){}
+    // pines del 0 al 7 se configuran como outputs
+    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+
+    // se utilizara el puerto E para los leds rojos PINES 1,2,3,4
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)){}
+    // pines del 0 al 7 se configuran como outputs
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4);
+
+    // loop principal
+    while (1) {
+        //prueba leds rojas
+        if ((GPIOPinRead (GPIO_PORTA_BASE,GPIO_PIN_5) & 0x20)==0) {
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, 0x1E);
+        }
+        else {
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4, 0x00);
+        }
+        // prueba leds verdes
+        // prueba 7 seg
+        SysCtlDelay (5000000); // DELAY 0.5s
+    }
 }
